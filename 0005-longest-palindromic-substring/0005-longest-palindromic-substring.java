6ecf3d9e3621d781bce3cbefb8977f61928c8db6
Krishankant Saraswat <122577473+KishankantSaraswat@users.noreply.github.com>
2024-11-05 11:23:40 +0530

    Time: 19 ms (60.27%), Space: 44.2 MB (64.93%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,5 +1,32 @@
 class Solution {
     public String longestPalindrome(String s) {
-        
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        String longestPalindrome = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            // Check for odd length palindromes
+            String oddPalindrome = expandAroundCenter(s, i, i);
+            if (oddPalindrome.length() > longestPalindrome.length()) {
+                longestPalindrome = oddPalindrome;
+            }
+
+            // Check for even length palindromes
+            String evenPalindrome = expandAroundCenter(s, i, i + 1);
+            if (evenPalindrome.length() > longestPalindrome.length()) {
+                longestPalindrome = evenPalindrome;
+            }
+        }
+
+        return longestPalindrome;
+    }
+    private static String expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
     }
 }
