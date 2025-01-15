4dcfd2f75c2555083e951559a8715df67db9c59a
Krishankant Saraswat <122577473+KishankantSaraswat@users.noreply.github.com>
2025-01-15 11:30:25 +0530

    Time: 11 ms (42.52%), Space: 48 MB (59.85%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,27 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> lst = new ArrayList<>();
-        int n = nums.length;
-        int threshold = n / 3;
+        List<Integer> lst=new ArrayList<>();
+        int k=nums.length/3;
+        Map<Integer, Integer> map =new HashMap<>();
+        for(int n:nums){
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
 
-        for (int i = 0; i < n; i++) {
-            if (!lst.contains(nums[i])) {  // Check if the number is already in the list
-                int count = 0;  // Reset count for the new element
-                // Count occurrences of nums[i]
-                for (int j = 0; j < n; j++) {
-                    if (nums[j] == nums[i]) {
-                        count++;
-                    }
-                }
-                // If count exceeds threshold, add it to the list
-                if (count > threshold) {
-                    lst.add(nums[i]);
-                }
+        for(int n:map.keySet()){
+            if(map.get(n)>k){
+                lst.add(n);
             }
         }
         return lst;
+        
     }
 }
