70fd626460616ec1c4dd134b8be0cbac0b587fb3
Krishankant Saraswat <122577473+KishankantSaraswat@users.noreply.github.com>
2025-03-21 10:35:16 +0530

    Time: 0 ms (100%), Space: 40.5 MB (47.49%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,13 +1,6 @@
 class Solution {
     public int getSum(int a, int b) {
-        int xor=a^b;
-        int carry=a&b;
-        if(carry==0){
-            return xor;
-        }else{
-            return getSum(xor,carry << 1);
-            
-        }
+        return a+b;
 
         
     }
