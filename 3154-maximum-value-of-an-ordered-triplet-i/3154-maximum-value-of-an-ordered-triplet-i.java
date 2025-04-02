5635caad844c666271587f716d9a217ee968e533
Krishankant Saraswat <122577473+KishankantSaraswat@users.noreply.github.com>
2025-04-03 03:31:54 +0530

    Time: 3 ms (53.4%), Space: 41.8 MB (86.56%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.java
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.java
@@ -1,16 +1,34 @@
 class Solution {
     public long maximumTripletValue(int[] nums) {
-        int n = nums.length;
-        long maxTripletValue = 0;
-
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    maxTripletValue = Math.max(maxTripletValue, (long) (nums[i] - nums[j]) * nums[k]);
+        long sum=0;
+        int n=nums.length;
+        long max=sum;
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-1;j++){
+                for(int k=j+1;k<n;k++){
+                    max = Math.max(max, (long) (nums[i] - nums[j]) * nums[k]);
                 }
             }
         }
-
-        return maxTripletValue;
+       
+        return max;
+        
+    
     }
 }
+// class Solution {
+//     public long maximumTripletValue(int[] nums) {
+//         int n = nums.length;
+//         long maxTripletValue = 0;
+
+//         for (int i = 0; i < n - 2; i++) {
+//             for (int j = i + 1; j < n - 1; j++) {
+//                 for (int k = j + 1; k < n; k++) {
+//                     maxTripletValue = Math.max(maxTripletValue, (long) (nums[i] - nums[j]) * nums[k]);
+//                 }
+//             }
+//         }
+
+//         return maxTripletValue;
+//     }
+// }
