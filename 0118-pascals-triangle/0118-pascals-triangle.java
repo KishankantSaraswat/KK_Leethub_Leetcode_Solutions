e9f013aeffdff037d44eb805394b24cfa29980cf
Krishankant Saraswat <122577473+KishankantSaraswat@users.noreply.github.com>
2025-05-20 08:45:06 +0530

    Time: 1 ms (84.99%), Space: 42.3 MB (25.35%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,25 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < numRows; i++) { // Start with 0-based indexing
-            List<Integer> lst = new ArrayList<>();
-            for (int j = 0; j <= i; j++) { // Iterate from 0 to i (inclusive)
-                int r = combination(i, j);
-                lst.add(r);
+        List<List<Integer>>res=new ArrayList<>();
+        int [][] arr=new int[numRows][numRows];
+        for(int i=0;i<numRows;i++){
+            for(int j=0;j<i+1;j++){
+                if(j==0 || j==i){
+                    arr[i][j]=1;
+                }
+                else{
+                    arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+                }
+            }
+        }
+
+        for(int i=0;i<arr.length;i++){
+            List<Integer>lst=new ArrayList<>();
+            for(int j=0;j<i+1;j++){
+                lst.add(arr[i][j]);
             }
             res.add(lst);
         }
         return res;
+        
     }
-
-    public int combination(int n, int r) {
-        long result = 1; // Use long to avoid overflow
-        for (int i = 0; i < r; i++) {
-            result = result * (n - i) / (i + 1); // Iterative computation
-        }
-        return (int) result; // Cast back to int as output
-    }
+    
 }
